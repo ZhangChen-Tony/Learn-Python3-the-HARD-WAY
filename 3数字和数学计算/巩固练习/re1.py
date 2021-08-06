ea415b4bf4ed480992aa5ca3199b7497768ef5b7
Tony-ZhangChen <tonyzhang3141@163.com>
2021-08-06 20:56:48 +0800

    I finishe re1!

--- 3数字和数学计算/巩固练习/re1.py
+++ 3数字和数学计算/巩固练习/re1.py
@@ -9,20 +9,28 @@ print("Roosters", 100 - 25 * 3 % 4)
 # print what's inside
 print("Now I will count the eggs:")
 
-# calculate 
+# calculate "number of eggs" by using the formula 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6, which is equivalent to 3 + 2 + 1 - 5 + (4 % 2) - (1 / 4) + 6
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# print what's inside
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# calculate whether it is true that 3 + 2 < 5 - 7, which is equivalent to (3 + 2) < (5 - 7)
 print(3 + 2 < 5 - 7)
 
+# calculate 3 + 2 and 5 - 7
 print("What is 3 + 2?", 3 + 2)
 print("What is 5 - 7?", 5 - 7)
 
+# wow, that is false qwq
 print("Oh, that's wht it's False.")
 
+# let's seek for more boolean values
 print("How about some more.")
 
+# five is greater than -2
 print("Is it greater?", 5 > -2)
+# five is greater than or equal to negative two
 print("Is it greater or equal?", 5 >= -2)
+# five is NOT less than or equal to negative two
 print("Is it less or equal?", 5 <= -2)
