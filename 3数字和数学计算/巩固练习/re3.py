a2d130f2b1db4c8686c298df61b7263239864b1d
Tony-ZhangChen <tonyzhang3141@163.com>
2021-08-06 18:56:32 +0800

    Added folder 3, did not finish re1.py qwq

---
+++ 3数字和数学计算/巩固练习/re3.py
@@ -0,0 +1,2 @@
+# If I need a TOEFL score of 110 to get into Cambridge, what is the average score I need in the four sections separately (full score of each four sections are 30, 30, 30, and 30)?
+print(110/4)
