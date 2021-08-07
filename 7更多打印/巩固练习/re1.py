c18e06187ff9e7d8c0499c5312296f2edc37df4b
Tony Zhang Chen <tonyzhang3141@163.com>
2021-08-07 12:26:38 +0800

    Created Folders on local server

--- 7更多打印/巩固练习/re1.py
+++ 7更多打印/巩固练习/re1.py
@@ -1,7 +1,7 @@
-print("Mary had a little lamb.")
-print("Its fleece was white as {}.".format('snow'))
-print("And everywhere that Mary went.")
-print("."*10) # what'd that do?
+print("Mary had a little lamb.")                                        # print what's inside
+print("Its fleece was white as {}.".format('snow'))                     # print with format
+print("And everywhere that Mary went.")                                 # print what's inside
+print("."*10) # what'd that do?                                         # print 10 dots
 
 end1 = "C"
 end2 = "h"
@@ -14,8 +14,8 @@ end8 = "u"
 end9 = "r"
 end10 = "g"
 end11 = "e"
-end12 = "r"
+end12 = "r"                                                             # define multiple variables
 
 # watch that comma at the end. try removing it to see what happens
-print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
-print(end7 + end8 + end9 + end10 + end11 + end12)
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')               # like the first 6 strings together, not changing line
+print(end7 + end8 + end9 + end10 + end11 + end12)                       # link the last 6 strings together
